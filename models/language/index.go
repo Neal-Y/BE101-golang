c4f34f1c0fe6f63201a8b4a166768628a4c7e2d7
neal <neal0420yang@gmail.com>
2024-03-21 23:39:28 +0800

    Language use var to make sure that msg only load once

--- models/language/index.go
+++ models/language/index.go
@@ -4,25 +4,27 @@ type Language interface {
 	GetMessage(event_name string) string
 }
 
+var ZnTWEventMessage = map[string]string{
+	"registerSuccess":     "註冊成功。歡迎加入我們的社群！",
+	"bookingSuccess":      "課程預定成功。期待見到你！",
+	"cancellationSuccess": "課程取消成功。希望在其他課程見到你。",
+}
+
+var EnUSEventMessage = map[string]string{
+	"registerSuccess":     "Registration successful. Welcome to our community!",
+	"bookingSuccess":      "Course successfully booked. We look forward to seeing you!",
+	"cancellationSuccess": "Course successfully cancelled. We hope to see you in other courses.",
+}
+
 type ZnTW struct{}
 type EnUS struct{}
 
 func (z ZnTW) GetMessage(event_name string) string {
-	message := map[string]string{
-		"registerSuccess":     "註冊成功。歡迎加入我們的社群！",
-		"bookingSuccess":      "課程預定成功。期待見到你！",
-		"cancellationSuccess": "課程取消成功。希望在其他課程見到你。",
-	}
-	return message[event_name]
+	return ZnTWEventMessage[event_name]
 }
 
 func (e EnUS) GetMessage(event_name string) string {
-	message := map[string]string{
-		"registerSuccess":     "Registration successful. Welcome to our community!",
-		"bookingSuccess":      "Course successfully booked. We look forward to seeing you!",
-		"cancellationSuccess": "Course successfully cancelled. We hope to see you in other courses.",
-	}
-	return message[event_name]
+	return EnUSEventMessage[event_name]
 }
 
 /*
