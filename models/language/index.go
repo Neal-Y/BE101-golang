40d3af32d992b872cedd3f1a0ad11574b5acfe90
neal <neal0420yang@gmail.com>
2024-03-22 19:51:18 +0800

    Language use some stupid thing to fixed lang

--- models/language/index.go
+++ models/language/index.go
@@ -4,27 +4,32 @@ type Language interface {
 	GetMessage(event_name string) string
 }
 
-var ZnTWEventMessage = map[string]string{
-	"registerSuccess":     "註冊成功。歡迎加入我們的社群！",
-	"bookingSuccess":      "課程預定成功。期待見到你！",
-	"cancellationSuccess": "課程取消成功。希望在其他課程見到你。",
-}
-
-var EnUSEventMessage = map[string]string{
-	"registerSuccess":     "Registration successful. Welcome to our community!",
-	"bookingSuccess":      "Course successfully booked. We look forward to seeing you!",
-	"cancellationSuccess": "Course successfully cancelled. We hope to see you in other courses.",
+var EventMessage = map[string]map[string]string{
+	"registerSuccess": {
+		"zh-tw": "註冊成功。歡迎加入我們的社群！",
+		"en-us": "Registration successful. Welcome to our community!",
+	},
+	"bookingSuccess": {
+		"zh-tw": "課程預定成功。期待見到你！",
+		"en-us": "Course successfully booked. We look forward to seeing you!",
+	},
+	"cancellationSuccess": {
+		"zh-tw": "課程取消成功。希望在其他課程見到你。",
+		"en-us": "Course successfully cancelled. We hope to see you in other courses.",
+	},
 }
 
 type ZnTW struct{}
 type EnUS struct{}
 
 func (z ZnTW) GetMessage(event_name string) string {
-	return ZnTWEventMessage[event_name]
+	lang := "zh-tw"
+	return EventMessage[event_name][lang]
 }
 
 func (e EnUS) GetMessage(event_name string) string {
-	return EnUSEventMessage[event_name]
+	lang := "en-us"
+	return EventMessage[event_name][lang]
 }
 
 /*
