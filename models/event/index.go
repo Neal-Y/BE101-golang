091a89bd75ac6be6d223896eb87881e55d40ec4c
neal <neal0420yang@gmail.com>
2024-03-25 22:42:57 +0800

    Event model 01

--- models/event/index.go
+++ models/event/index.go
@@ -11,22 +11,42 @@ type Event interface {
 }
 
 type Signup struct {
+	methods   []notifier.Notifier
 	studentID string
 }
 
 type BookClass struct {
+	methods []notifier.Notifier
 	classID string
 }
 
 type CancelClass struct {
+	methods []notifier.Notifier
 	classID string
 }
 
-func (s Signup) AddNotifier(notifier notifier.Notifier) {}
-func (s Signup) Trigger(user user.User)                 {}
+func (s Signup) AddNotifier(notifier notifier.Notifier) {
+	s.methods = append(s.methods, notifier)
+}
+func (s Signup) Trigger(user user.User) {
+	for _, notifier := range s.methods{
+		notifier.Notify(user, "Welcome to the class!")
+	}
+}
+//Notify(user user.User, message string) 
+func (b BookClass) AddNotifier(notifier notifier.Notifier) {
+	b.methods = append(b.methods, notifier)
+}
+func (b BookClass) Trigger(user user.User) {
+	for _, notifier := range b.methods{
+		notifier.Notify(user, "Class booked!")
+}
 
-func (b BookClass) AddNotifier(notifier notifier.Notifier) {}
-func (b BookClass) Trigger(user user.User)                 {}
-
-func (c CancelClass) AddNotifier(notifier notifier.Notifier) {}
-func (c CancelClass) Trigger(user user.User)                 {}
+func (c CancelClass) AddNotifier(notifier notifier.Notifier) {
+	c.methods = append(c.methods, notifier)
+}
+func (c CancelClass) Trigger(user user.User) {
+	for _, notifier := range c.methods{
+		notifier.Notify(user, "Class cancelled!")
+	}
+}
