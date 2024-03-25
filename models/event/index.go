9c71ab8d7c62015be5a15f38f74e2d15cfcce3bb
neal <neal0420yang@gmail.com>
2024-03-26 00:27:48 +0800

    Event use embeded struct to refactor

--- models/event/index.go
+++ models/event/index.go
@@ -11,62 +11,42 @@ type Event interface {
 	GetEventName() string
 }
 
-type Signup struct {
+type BaseEvent struct {
 	methods []notifier.Notifier
+}
+
+type Signup struct {
+	BaseEvent
 }
 
 type BookClass struct {
-	methods []notifier.Notifier
+	BaseEvent
 }
 
 type CancelClass struct {
-	methods []notifier.Notifier
+	BaseEvent
+}
+
+func (b *BaseEvent) AddNotifier(notifier notifier.Notifier) {
+	b.methods = append(b.methods, notifier)
+}
+
+func (b *BaseEvent) Trigger(user user.User, event Event) {
+	for _, notifier := range b.methods {
+		if msg, err := user.GetPreferredLanguage().GetMessage(event.GetEventName()); err == nil {
+			notifier.Notify(user, msg)
+		}
+	}
 }
 
 func (s Signup) GetEventName() string {
 	return "register"
 }
 
-func (s *Signup) AddNotifier(notifier notifier.Notifier) {
-	s.methods = append(s.methods, notifier)
-}
-
-func (s *Signup) Trigger(user user.User) {
-	for _, notifier := range s.methods {
-		if msg, err := user.GetPreferredLanguage().GetMessage(s.GetEventName()); err == nil {
-			notifier.Notify(user, msg)
-		}
-	}
-}
-
-func (b *BookClass) GetEventName() string {
+func (b BookClass) GetEventName() string {
 	return "booking"
 }
 
-func (b *BookClass) AddNotifier(notifier notifier.Notifier) {
-	b.methods = append(b.methods, notifier)
-}
-
-func (b *BookClass) Trigger(user user.User) {
-	for _, notifier := range b.methods {
-		if msg, err := user.GetPreferredLanguage().GetMessage(b.GetEventName()); err == nil {
-			notifier.Notify(user, msg)
-		}
-	}
-}
-
-func (c *CancelClass) GetEventName() string {
+func (c CancelClass) GetEventName() string {
 	return "cancellation"
-}
-
-func (c *CancelClass) AddNotifier(notifier notifier.Notifier) {
-	c.methods = append(c.methods, notifier)
-}
-
-func (c *CancelClass) Trigger(user user.User) {
-	for _, notifier := range c.methods {
-		if msg, err := user.GetPreferredLanguage().GetMessage(c.GetEventName()); err == nil {
-			notifier.Notify(user, msg)
-		}
-	}
 }
