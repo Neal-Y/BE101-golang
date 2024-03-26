3014d37188ce36dd6aa892d1afbfdf24aaf931ba
neal <neal0420yang@gmail.com>
2024-03-26 23:53:11 +0800

    Event implemented

--- models/event/Signup.go
+++ models/event/Signup.go
@@ -1,1 +1,24 @@
 package event
+
+import (
+	"be101_golang/models/notifier"
+	"be101_golang/models/user"
+)
+
+type Signup struct {
+	methods []notifier.Notifier
+}
+
+func (s Signup) GetEventName() string {
+	return "register"
+}
+
+func (s *Signup) AddNotifier(n notifier.Notifier) {
+	s.methods = append(s.methods, n)
+}
+
+func (s *Signup) Trigger(user user.User) {
+	for _, method := range s.methods {
+		method.Notify(user, s.GetEventName())
+	}
+}
