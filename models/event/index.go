5530a68d126d7786d78836120628704d99e521e8
neal <neal0420yang@gmail.com>
2024-03-27 13:49:04 +0800

    Event cherry pick

--- models/event/index.go
+++ models/event/index.go
@@ -8,25 +8,5 @@ import (
 type Event interface {
 	AddNotifier(notifier notifier.Notifier)
 	Trigger(user user.User)
+	GetEventName() string
 }
-
-type Signup struct {
-	studentID string
-}
-
-type BookClass struct {
-	classID string
-}
-
-type CancelClass struct {
-	classID string
-}
-
-func (s Signup) AddNotifier(notifier notifier.Notifier) {}
-func (s Signup) Trigger(user user.User)                 {}
-
-func (b BookClass) AddNotifier(notifier notifier.Notifier) {}
-func (b BookClass) Trigger(user user.User)                 {}
-
-func (c CancelClass) AddNotifier(notifier notifier.Notifier) {}
-func (c CancelClass) Trigger(user user.User)                 {}
